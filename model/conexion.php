99ddb0bfe5d8536e25bbf0e77f512e1299491aee
MariaCutipa <maria.c.br1031@gmail.com>
2023-05-27 21:46:03 -0500

    base datos

--- model/conexion.php
+++ model/conexion.php
@@ -1,11 +1,12 @@
 <?php 
 $contrasena = "AVNS_4_kzRPbMLHxIe24b7DG";
 $usuario = "doadmin";
-$nombre_bd = "Registro";
-
+$nombre_bd = "defaultdb";
+$puerto = "25060";
 try {
 	$bd = new PDO (
 		'mysql:host=db-mysql-nyc1-83352-do-user-14161302-0.b.db.ondigitalocean.com;
+		port='.$puerto.';
 		dbname='.$nombre_bd,
 		$usuario,
 		$contrasena,
